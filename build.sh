96171f7f7242e7f17dc25bf8997e909d38c433d3
oxxide216 <vie.devr@gmail.com>
2025-09-29 13:54:05 +0300

    Updated build script

--- build.sh
+++ build.sh
@@ -12,9 +12,14 @@ AETHER_COMPILER_SRC="$(find libs/aether/compiler-src -name "*.c")"
 LEXGEN_SRC="$(find libs/lexgen/runtime-src -name "*.c")"
 
 lexgen libs/aether/compiler-src/grammar.h libs/aether/compiler-src/grammar.lg
-cc -o aetherc -Wall -Wextra -Ilibs/aether/ir-src -Ilibs -Ilibs/aether/vm-include \
-  -Ilibs/aether/ir-include -z execstack "${@:1}" $AETHER_COMPILER_SRC \
-  $AETHER_IR_SRC $LEXGEN_SRC
+cc -o aetherc -Wall -Wextra -Ilibs/aether/ir-src -Ilibs -Ilibs/aether/ir-include \
+  -z execstack "${@:1}" $AETHER_COMPILER_SRC $AETHER_IR_SRC $LEXGEN_SRC
+
+AETHER_TEST_VM_SRC="$(find libs/aether/test-vm-src -name "*.c")"
+
+cc -o aether-vm -Wall -Wextra -Ilibs/aether/ir-src -Ilibs -Ilibs/aether/vm-include \
+  -Ilibs/aether/ir-include -z execstack "${@:1}" $AETHER_TEST_VM_SRC \
+  $AETHER_VM_SRC $AETHER_IR_SRC
 
 cc -o inc $CFLAGS $LDFLAGS "${@:1}" $CLIENT_SRC $INTP_SRC $AETHER_IR_SRC $AETHER_VM_SRC
 cc -o ins $CFLAGS $LDFLAGS "${@:1}" $SERVER_SRC $INTP_SRC $AETHER_IR_SRC $AETHER_VM_SRC
